d8ad2e612e24daba596484f5ed514b9ace84aa09
Lucasz <zhouzihang97@163.com>
2023-12-23 12:15:03 +0800

    Initial Commit

---
+++ waterReminder/waterReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  waterReminderApp.swift
+//  waterReminder
+//
+//  Created by Zhou Lucas on 2023/12/23.
+//
+
+import SwiftUI
+
+@main
+struct waterReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
